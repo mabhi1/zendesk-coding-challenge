45e85ec1cf9a05badaef12bd54eb1b4856a7cf75
Abhishek Mishra <amishr1@stevens.edu>
2021-11-20 00:42:58 -0500

    error-handling

--- routes/tickets.js
+++ routes/tickets.js
@@ -4,30 +4,57 @@ const axios = require("axios");
 const url = "https://zccabhishek.zendesk.com/api/v2/tickets/";
 
 router.get("/", async (req, res) => {
-  const { data } = await axios.get(url, {
-    auth: {
-      username: "amishr1@stevens.edu",
-      password: "Abhishek@1995",
-    },
-  });
-  if (data.tickets) {
-    res.render("index", {
-      data: data.tickets,
+  let error = "";
+  try {
+    const { data } = await axios.get(url, {
+      auth: {
+        username: "amishr1@stevens.edu",
+        password: "Abhishek@1995",
+      },
+    });
+    if (data.ticket) {
+      res.render("index", {
+        data: data.tickets,
+      });
+    } else {
+      throw { response: { data: { error: "No data found" } } };
+    }
+  } catch (e) {
+    if (e.response.data.error.title) {
+      error = e.response.data.error.title;
+    } else {
+      error = e.response.data.error;
+    }
+    res.status(500).render("error", {
+      error: error,
     });
   }
 });
 
 router.get("/:id", async (req, res) => {
-  const { data } = await axios.get(url + req.params.id, {
-    auth: {
-      username: "amishr1@stevens.edu",
-      password: "Abhishek@1995",
-    },
-  });
-  console.log(data);
-  if (data.ticket) {
-    res.render("ticket", {
-      data: data.ticket,
+  let error = "";
+  try {
+    const { data } = await axios.get(url + req.params.id, {
+      auth: {
+        username: "amishr1@stevens.edu",
+        password: "Abhishek@1995",
+      },
+    });
+    if (data.ticket) {
+      res.render("ticket", {
+        data: data.ticket,
+      });
+    } else {
+      throw { response: { data: { error: "No data found" } } };
+    }
+  } catch (e) {
+    if (e.response.data.error.title) {
+      error = e.response.data.error.title;
+    } else {
+      error = e.response.data.error;
+    }
+    res.status(500).render("error", {
+      error: error,
     });
   }
 });
