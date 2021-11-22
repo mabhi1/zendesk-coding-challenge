a0340018602301fa758855808fb015113e264c13
Abhishek Mishra <amishr1@stevens.edu>
2021-11-22 05:40:23 -0500

    pagination

--- routes/tickets.js
+++ routes/tickets.js
@@ -5,16 +5,28 @@ const url = "https://zccabhishek.zendesk.com/api/v2/tickets/";
 
 router.get("/", async (req, res) => {
   let error = "";
+  let next_page = true;
+  let previous_page = true;
+  page = parseInt(req.query.page);
+  if (!page) page = 1;
   try {
-    const { data } = await axios.get(url, {
+    const { data } = await axios.get(url + "?page=" + page + "&per_page=25", {
       auth: {
         username: "amishr1@stevens.edu",
         password: "Abhishek@1995",
       },
     });
-    if (data.ticket) {
+    if (data.next_page == null) next_page = false;
+    if (data.previous_page == null) previous_page = false;
+    if (data.tickets) {
       res.render("index", {
         data: data.tickets,
+        next_page: next_page,
+        previous_page: previous_page,
+        next: page + 1,
+        previous: page - 1,
+        count: data.count,
+        len: data.tickets.length,
       });
     } else {
       throw { response: { data: { error: "No data found" } } };
